20806a54336d2565047026e0c25542d39b2bd2d2
foliwe <foliwe@gmail.com>
2020-09-26 01:12:51 +0100

    sassemail

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'paul_smith@sassinvestigators.org'
+  layout 'mailer'
+end
---
+++ app/mailers/target_mailer.rb
@@ -0,0 +1,9 @@
+class TargetMailer < ApplicationMailer
+    default from: 'paul_smith@sasinvestigators.com'
+ 
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+end
